17f5102e8d6de9abae0911dab0674254b6a7c87a
Minh <minh@xendit.co>
2022-05-17 22:38:18 +0700

    Update brands and add request to handle, offset, sort, search, description, name

--- index.js
+++ index.js
@@ -37,9 +37,58 @@ const brands = [
         image: "https://www.suzuki.co.jp/wp-content/uploads/2019/01/Suzuki-Logo-1.png",
         country: "Japan"
     },
+    {
+        id: 5,
+        name: "Kawasaki",
+        description: "Kawasaki is a Japanese brand of automobiles and motorcycles. It is the largest Japanese brand after Honda.",
+        image: "https://www.kawasaki.co.jp/wp-content/uploads/2019/01/Kawasaki-Logo-1.png",
+        country: "Japan"
+    },
+    {
+        id: 6,
+        name: "Yamaha",
+        description: "Yamaha is a Japanese brand of automobiles and motorcycles. It is the largest Japanese brand after Honda.",
+        image: "https://www.yamaha.co.jp/wp-content/uploads/2019/01/Yamaha-Logo-1.png",
+        country: "Japan"
+    },
+    {
+        id: 7,
+        name: "Pepsi",
+        description: "Pepsi is a Japanese brand of automobiles and motorcycles. It is the largest Japanese brand after Honda.",
+        image: "https://www.suzuki.co.jp/wp-content/uploads/2019/01/Pepsi-Logo-1.png",
+        country: "USA"
+    },
+    {
+        id: 8,
+        name: "CocaCola",
+        description: "CocaCola is a Japanese brand of automobiles and motorcycles. It is the largest Japanese brand after Honda.",
+        image: "https://www.suzuki.co.jp/wp-content/uploads/2019/01/Suzuki-Logo-1.png",
+        country: "England"
+    },
+    {
+        id: 9,
+        name: "Iphone",
+        description: "Iphone is a Japanese brand of automobiles and motorcycles. It is the largest Japanese brand after Honda.",
+        image: "https://www.suzuki.co.jp/wp-content/uploads/2019/01/Suzuki-Logo-1.png",
+        country: "USA"
+    },
+    {
+        id: 10,
+        name: "Giày Thượng Đình",
+        description: "Giày Thượng Đình is a Japanese brand of automobiles and motorcycles. It is the largest Japanese brand after Honda.",
+        image: "https://www.suzuki.co.jp/wp-content/uploads/2019/01/Suzuki-Logo-1.png",
+        country: "Việt Nam"
+    },
+    {
+        id: 11,
+        name: "Lexus",
+        description: "Iphone is a Japanese brand of automobiles and motorcycles. It is the largest Japanese brand after Honda.",
+        image: "https://www.suzuki.co.jp/wp-content/uploads/2019/01/Suzuki-Logo-1.png",
+        country: "Campuchia"
+    },
 ]
 
-// api/brands?country=Japan
+// api/brands?country=Japan&limit=1
 app.get('/api/brands', (req, res) => {
     // Request Queries
     const countryQuery = req.query.country;
@@ -56,8 +105,12 @@ app.get('/api/brands', (req, res) => {
     });
 })
 
-// api/brands/:id
-
+// Handle limit and country
+// Handle offset
+// Handle sort
+// Handle search
+// Handle description
+// Handle name
 
 app.listen(port, () => {
     console.log(port)
